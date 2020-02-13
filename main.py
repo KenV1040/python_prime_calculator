5a064eef0747747bbd59b7595a7bc5ee6f714e81
Ken <vuken30227@gmail.com>
2020-02-13 11:03:50 +0000

    Added extra print statement

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ import time
 def main():
     # I control all my tests here
     print("Prime calculator!")
-    test_numbers = [5000,10000]
+    #test_numbers = [500,1000,1500,2000,2500,5000,10000]
+    #test_numbers = [20000,30000,40000,50000]
+    test_numbers = [100]
     # The array of numbers is the maximum number I'll calculate prime numbers up to.
     # Also, I repeat this 3 times to get an average time. This is to ensure reliable results
     for number in test_numbers:
-        for i in range(3):
+        for i in range(1):
             time_took = primeSieve(2, number)
             print("{} run".format(i))
             print("Number: {num} Time took: {time}".format(num=number, time= time_took))
@@ -20,7 +22,6 @@ def main():
 def primeSieve(begin, end):
     i = begin
     n = end
-    nonPrime = []
     prime = []
     start_time = time.time()
     while (i < n):
@@ -31,17 +32,11 @@ def primeSieve(begin, end):
                 isContinue = True
                 break
         if isContinue: continue
-    
-        
-        temp_i = i
-        """ while (temp_i < n):
-            nonPrimeNumber = temp_i * i
-            nonPrime.append(nonPrimeNumber)
-            temp_i = temp_i + 1 """
         prime.append(i)
         i = i + 1
 
     time_took = time.time() - start_time
+    print(prime)
     print("Length of prime numbers: {}".format(len(prime)))
     return time_took
 
